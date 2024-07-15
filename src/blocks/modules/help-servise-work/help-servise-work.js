99c4c376b3919e6190c1b8935bf0f37518c70272
ren4 <pro100ren4@gmail.com>
2024-07-15 05:10:55 +0300

    feat: make help service work section layout

---
+++ src/blocks/modules/help-servise-work/help-servise-work.js
@@ -0,0 +1,13 @@
+modules.define('help-servise-work', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
