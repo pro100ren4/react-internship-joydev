4902bc567bca54e9414cb0fef550cdd245967fed
ren4 <pro100ren4@gmail.com>
2024-10-25 08:59:57 +0300

    feat: add layout for help-filter section

---
+++ src/blocks/modules/help-search-filter/help-search-filter.js
@@ -0,0 +1,13 @@
+modules.define('help-search-filter', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
