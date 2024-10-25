16206f6d6dc717dca790fb8657a16036f5ee854e
ren4 <pro100ren4@gmail.com>
2024-10-25 11:29:00 +0300

    feat: add help solutions extra features layout

---
+++ src/blocks/modules/help-extra-features/help-extra-features.js
@@ -0,0 +1,13 @@
+modules.define('help-extra-features', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
